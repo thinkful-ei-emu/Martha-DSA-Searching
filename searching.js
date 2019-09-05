f4768929b73c62d5bac208326fb939bd262adad1
Martha Plantz <maplantz89@gmail.com>
2019-09-05 14:27:06 -0700

    understanding searching more

--- searching.js
+++ searching.js
@@ -46,6 +46,7 @@ function binarySearch(array, value, start, end){
   }
 }
 
+//1
 //console.log(binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18], 8));
 /**
  * step 1: 0 + 10 = 10/2 = 5 => [3 -> 12]
@@ -64,6 +65,8 @@ function binarySearch(array, value, start, end){
  * base case => -1
  */
 
+//2 other file
+
 //3. 
 /**
  * Imgaine you are looking for a book in a library with a Dewey 
@@ -71,12 +74,66 @@ function binarySearch(array, value, start, end){
  * process as a search algorithm? Implement your algorithm to 
  * find a book whose Dewey and book title is provided.
  * 
- * This would be similar to binary searching, you first look for 
- * the correct high level category find that number 
- * 
  * Then find the next deeper level category
+ *  cat1 cat2 cat3 cat4 -> go to specified category
+ * [b1, b2, b3, b4, b5, b6, b7, b8] 
+  * each book belongs to many growing categories 
+  * in order to find a specific book start at the top level and 
+  * eliminate groups of books along the way until you have gotten 
+  * specific enough that you only have 1 book left. 
+  * 
+  *   more specifically if you list of all books is: (assuming the library sorts their books)
+  * [110.2, 123.4, 543.4, 300.2]
+  *   and you are searching for 123.4
+  * 
+  * check the first numbers of each book and only consider books with the same 
+  * first number 
+  * then same for second number until no numbers left
  * 
  */
+
+
+//4. 
+
+//in-order - (left child, parent, right child) 
+//pre-order (parent, left child, right child) 
+//post-order (left child, right child, parent)
+
+/**
+ * Given a binary search tree whose in-order and pre-order 
+ * traversals are respectively 14 15 19 25 27 35 79 89 90 91 
+ * and 35 25 15 14 19 27 89 79 91 90. What would be its postorder 
+ * traversal?
+ *  
+ * in-order : 14, 15, 19, 25,   27,  35, 79, 89, 90, 91
+ * pre-order : 35, 25, 15, 14, 19, 27, 89, 79, 91, 90
+ * 
+ *             
+ *           
+ *             27         
+ *      /              \ 
+ *    15               90
+ *   / \              /     \
+ * 14   19           79     91
+ *        \          /\
+ *        25        35 89
+ *                  
+ *                 
+ * 
+ * 2) The post order traversal of a binary search tree is 
+ * 5 7 6 9 11 10 8. What is its pre-order traversal?
+ * 
+ * 5 6 7 8 9 10 11
+ * 
+ *        8
+ *   6         10
+ * 5  7      9   11
+ * 
+ * 
+ *    6 5 7 8 10 9 11
+ * 
+ */
+
 
 class _Node{
   constructor(value){
@@ -239,59 +296,107 @@ class BinarySearchTree{
     }
     return values;
   }
+
+  dsfPreOrder(){
+    //preOrder
+    console.log(this.key);
+    if(this.left){
+      this.left.dsfPreOrder();
+    }
+    if(this.right){
+      this.right.dsfPreOrder();
+    }
+  }
+  dsfInOrder(){
+    if(this.left){
+      this.left.dsfInOrder();
+    }
+    console.log(this.key);
+    if(this.right){
+      this.right.dsfInOrder();
+    }
+  }
+
+  dsfPostOrder(){
+    if(this.left){
+      this.left.dsfPostOrder();
+    }
+    if(this.right){
+      this.right.dsfPostOrder();
+    }
+    console.log(this.key);
+  }
 }
-
-//4. 
-
-//in-order - (left child, parent, right child) 
-//pre-order (parent, left child, right child) 
-//post-order (left child, right child, parent)
-
-/**
- * Given a binary search tree whose in-order and pre-order 
- * traversals are respectively 14 15 19 25 27 35 79 89 90 91 
- * and 35 25 15 14 19 27 89 79 91 90. What would be its postorder 
- * traversal?
- *  
- * in-order : 14, 15, 19, 25,   27,  35, 79, 89, 90, 91
- * pre-order : 35, 25, 15, 14, 19, 27, 89, 79, 91, 90
- * 
- *             
- *           
- *             27         
- *      /              \ 
- *    15               90
- *   / \              /     \
- * 14   19           79     91
- *        \          /\
- *        25        35 89
- *                  
- *                 
- * 
- * 2) The post order traversal of a binary search tree is 
- * 5 7 6 9 11 10 8. What is its pre-order traversal?
- * 
- * 5 6 7 8 9 10 11
- * 
- *        8
- *   6         10
- * 5  7      9   11
- * 
- * 
- *    6 5 7 8 10 9 11
- * 
- */
 
 
 function main(){
   let bst = new BinarySearchTree();
-  //console.log(bst.depthFirstSearch([25, 15, 50, 10, 24, 35, 70, 4, 12, 18, 31, 44, 66, 90, 22]));
+  bst.insert(25);
+  bst.insert(15);
+  bst.insert(50);
+  bst.insert(10);
+  bst.insert(24);
+  bst.insert(35);
+  bst.insert(70);
+  bst.insert(4);
+  bst.insert(12);
+  bst.insert(18);
+  bst.insert(31);
+  bst.insert(44);
+  bst.insert(66);
+  bst.insert(90);
+  bst.insert(22);
+  return bst;
 }
 
-console.log(main());
+let tree = main();
+//console.log(tree.dsfPreOrder());
+//console.log(tree.dsfInOrder());
+//console.log(tree.dsfPostOrder());
 
 
+function nextCommanding(tree){
+  console.log(tree.key);
+  if(tree.left){
+    console.log(tree.left.key);
+  }
+  if(tree.right){
+    console.log(tree.right.key);
+  }
+  if(tree.left && tree.right){
+    nextCommanding(tree.left);
+    nextCommanding(tree.right);
+  }
+}
+let rank = new BinarySearchTree();
+rank.insert(5);
+rank.insert(2);
+rank.insert(3);
+rank.insert(1);
+rank.insert(4);
+rank.insert(7);
+rank.insert(6);
+rank.insert(9);
 
+
+function maxProfit(){
+
+}
+
+console.log(maxProfit([128, 97, 121, 123, 98, 97, 105]));
+
+//s m t w th f s
+/**
+ * want the index of the largest and smallest 
+ */
+
+/**
+ * Rank:           5
+ *             2      7     
+ *           1   3   6  9
+ *                4
+ */
+console.log(nextCommanding(rank));
 
 
 
